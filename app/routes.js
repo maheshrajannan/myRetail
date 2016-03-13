a8cc594f02f03c7990e160b6b06c5ff72f532042
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2016-03-12 20:41:14 -0600

    get product followed by price info

--- app/routes.js
+++ app/routes.js
@@ -4,46 +4,32 @@ var ProductApiClient = require('./productApiClient');
 
 function getProduct(res,inProductId){
 	console.log('getProduct inProductId:' + inProductId);
-	PriceInfo.find(
-		{productId:inProductId},
-		function(err, priceInfos) {
-			var priceInfo = priceInfos[0];
-			console.log('foundPriceInfo ' + util.inspect(priceInfo,false,null));
 
-			//TODO change to productPriceInfos
-			var priceProductInfos =[];
-			var itemsProcessed = 0;
 
-			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-			if (err)
-				res.send(err)
+	//TODO: add not null function.
+	//TODO: check in.
+	//TODO: format nicely.
+	//TODO: unit testing.
+	//TODO: callback mayhem for each price info, call forEachCallback, 
+	//TODO: add the authentication param
+	var priceProductInfo;
+	ProductApiClient.getProduct(inProductId,function(product) {
+		console.log('getProduct updating product' + util.inspect(product,false,null));
+		//Example response: {"id":13860428,"name":"The Big Lebowski (Blu-ray) (Widescreen)","current_price":{"value": 13.49,"currency_code":"USD"}}
+		//TODO: change to productPriceInfo
+		//var priceProductInfo = {id:inProductId,name:product.productName,priceInfo:priceInfo};
+		//TODO: null check on productId
+		PriceInfo.find(
+			{productId:inProductId},
+			function(err, foundPriceInfos) {
+				if (err)
+					res.send(err)
+				console.log('foundPriceInfo ' + util.inspect(foundPriceInfos,false,null));
+				priceProductInfo = {id:inProductId,name:product.productName,priceInfos:foundPriceInfos};
+				res.json(priceProductInfo);
+			});
+	});
 
-			//TODO: add not null function.
-			//TODO: check in.
-			//TODO: format nicely.
-			//TODO: unit testing.
-			//TODO: callback mayhem for each price info, call forEachCallback, 
-			//TODO: add the authentication param
-				var product;
-				ProductApiClient.getProduct(priceInfo.productId,function(outProduct) {
-					product = outProduct;
-					console.log('getProduct updating product' + util.inspect(product,false,null));
-					//var priceProductInfo = {priceInfo:priceInfo ,product : ProductApiClient.getProduct(priceInfo.productId)}
-					//Example response: {"id":13860428,"name":"The Big Lebowski (Blu-ray) (Widescreen)","current_price":{"value": 13.49,"currency_code":"USD"}}
-					//TODO: change to productPriceInfo
-					var priceProductInfo = {id:product.productId,name:product.productName,priceInfo:priceInfo};
-					//TODO: null check on productId
-					console.log('getProduct priceInfo' + util.inspect(priceProductInfo,false,null));
-					priceProductInfos.push(priceProductInfo);
-					//return priceInfo2;
-					 itemsProcessed++;
-					 console.log('getPriceInfo itemsProcessed' + itemsProcessed + 'priceInfos.length' + priceInfos.length);
-					 if(itemsProcessed == priceInfos.length) {
-					 	console.log('getPriceInfo returing');
-					 	return res.json(priceProductInfos);
-					 }
-				});
-		});
 };
 
 module.exports = function(app) {
