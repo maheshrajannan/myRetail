53bb0fce167a226af1ff2b91aa4b9f96d811ae96
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2016-03-12 21:29:05 -0600

    not null checks

--- app/ProductApiClient.js
+++ app/ProductApiClient.js
@@ -21,27 +21,37 @@ productApiClient.registerMethod("getProductInfo", productApiUrl, "GET");
 
 
 var getProduct = function (inProductId,callback) {
+	var product;
 	console.log('inProductId:' + inProductId);
-	args.path.productId=inProductId;
-	var product = { productId : inProductId ,productName : 'NA'};
-	console.log('productApiClient.methods.getProductInfo'+util.inspect(productApiClient.methods.getProductInfo,false,null));
-	//DONE:move this to a separate file
-	//productApiClient.get(productApiUrl, function (data, response) {
-		productApiClient.methods.getProductInfo(args, function (data, response) {	
-			// parsed response body as js object 
-			//TODO null checks on each
-			//TODO: make the field to read as configurable
-			//console.log('response'+util.inspect(data.product_composite_response.request_attributes));
-			//console.log('data'+util.inspect(data,false,null));
-			//console.log('data.product_composite_response'+util.inspect(data.product_composite_response,false,null));
-			//console.log('data.product_composite_response.items'+util.inspect(data.product_composite_response.items));
-			product.productName = data.product_composite_response.items[0].online_description.value;
-			//TODO: replace with better logger.
-			console.log('id:' + inProductId + 'data from productAPI' + util.inspect(data.product_composite_response.items[0].online_description,false,null));
-			// raw response 
-			//console.log('raw response from product api' + response);
-			return callback(product);
-		});
+	if(!util.isNullOrUndefined(inProductId)) {
+		args.path.productId=inProductId;
+		console.log('productApiClient.methods.getProductInfo'+util.inspect(productApiClient.methods.getProductInfo,false,null));
+		//DONE:move this to a separate file
+		//productApiClient.get(productApiUrl, function (data, response) {
+			productApiClient.methods.getProductInfo(args, function (data, response) {
+				if(!util.isNullOrUndefined(data) && !util.isNullOrUndefined(data.product_composite_response.items[0].online_description)) {
+					product = { productId : inProductId ,productName : 'NA'};
+					// parsed response body as js object 
+					//TODO null checks on each
+					//TODO: make the field to read as configurable
+					//console.log('response'+util.inspect(data.product_composite_response.request_attributes));
+					//console.log('data'+util.inspect(data,false,null));
+					//console.log('data.product_composite_response'+util.inspect(data.product_composite_response,false,null));
+					//console.log('data.product_composite_response.items'+util.inspect(data.product_composite_response.items));
+					product.productName = data.product_composite_response.items[0].online_description.value;
+					//TODO: replace with better logger.
+					console.log('id:' + inProductId + 'data from productAPI' + util.inspect(data.product_composite_response.items[0].online_description,false,null));
+					// raw response 
+					//console.log('raw response from product api' + response);
+					return callback(product);
+				} else {
+					return callback(product);
+				}	
+				//TODO: handle error call back.
+			});
+	} else {
+		return callback(product);
+	}
 };
 
 module.exports.getProduct = getProduct;
