209d6ffad4cf4faea3e38e2b9cbe8182318509e1
Mahesh Rajannan <mahesh.rajannan@gmail.com>
2016-03-13 23:13:58 -0500

    fixed the threading scope issue.

--- app/ProductApiClient.js
+++ app/ProductApiClient.js
@@ -1,35 +1,41 @@
 //DONE: move this to a file
 //ProductApi Client interacts with the products api service to just get the product name
 
-var Client = require('node-rest-client').Client;
-var productApiClient = new Client();
-//var productApiUrl = 'https://api.target.com/products/v3/13860428?fields=descriptions&id_type=TCIN&key=43cJWpLjH8Z8oR18KdrZDBKAgLLQKJjz';
-var productApiUrl = 'https://api.target.com/products/v3/${productId}';
 var util = require('util');
-
-//TODO: move this to properties ?
-var args = {
-	path: { "productId": 1 }, // path substitution var 
-	//TODO: if you have time parameterize this.
-	parameters: { fields : "descriptions", id_type : "TCIN" , key : "43cJWpLjH8Z8oR18KdrZDBKAgLLQKJjz" } // query parameter substitution vars 
-};
-
-// registering remote methods 
-productApiClient.registerMethod("getProductInfo", productApiUrl, "GET");
-//console.log('data'+util.inspect(args));
 
 
 var getProduct = function (inProductId,callback) {
 	var product;
 	console.log('inProductId:' + inProductId);
 	if(!util.isNullOrUndefined(inProductId)) {
+		//TODO: there is some threading problem with this API, so initializing this every time and using closure .
+		var productApiUrl = 'https://api.target.com/products/v3/${productId}';
+
+		//TODO: move this to properties ?
+		var args = {
+			path: { "productId": 1 }, // path substitution var 
+			//TODO: if you have time parameterize this.
+			parameters: { fields : "descriptions", id_type : "TCIN" , key : "43cJWpLjH8Z8oR18KdrZDBKAgLLQKJjz" } // query parameter substitution vars 
+		};
+
+		var Client = require('node-rest-client').Client;
+		var productApiClient = new Client();
 		args.path.productId=inProductId;
+		// registering remote methods 
+		productApiClient.registerMethod("getProductInfo", productApiUrl, "GET");
+		
+		//console.log('data'+util.inspect(args));
 		console.log('productApiClient.methods.getProductInfo'+util.inspect(productApiClient.methods.getProductInfo,false,null));
 		//DONE:move this to a separate file
 		//productApiClient.get(productApiUrl, function (data, response) {
 			productApiClient.methods.getProductInfo(args, function (data, response) {
+				//TODO: move this to a separate file.
 				console.log('data'+util.inspect(data,false,null));
-				if(!util.isNullOrUndefined(data) && !util.isNullOrUndefined(data.product_composite_response.items[0].online_description)) {
+				if(!util.isNullOrUndefined(data) && 
+					!util.isNullOrUndefined(data.product_composite_response) &&  
+					!util.isNullOrUndefined(data.product_composite_response.items) &&  
+					!util.isNullOrUndefined(data.product_composite_response.items[0]) &&  
+				 	!util.isNullOrUndefined(data.product_composite_response.items[0].online_description) ) {
 					product = { productId : inProductId ,productName : 'NA'};
 					// INFO: data is parsed response body as js object 
 					//TODO: make the field to read as configurable
